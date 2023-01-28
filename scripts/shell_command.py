7704a9978d957bf3cc83cb79e78313f4321be603
Depish <depish.eskry@yandex.ru>
2023-01-28 12:05:13 +0300

    Разделил функции на выполняемые в указанной директории и нет

--- scripts/shell_command.py
+++ scripts/shell_command.py
@@ -4,7 +4,7 @@ import platform
 import sys
 
 
-def shell_command(directory, program, args=None, no_errors=False):
+def shell_command(program, args=None, no_errors=False):
     if args is None:
         args = []
     if isinstance(args, str):
@@ -14,18 +14,14 @@ def shell_command(directory, program, args=None, no_errors=False):
         args = [f'"{arg}"' for arg in args]
     args.insert(0, program)
 
-    old_dir = os.getcwd()
-    os.chdir(old_dir)
-
     ret_code = subprocess.call(args, stderr=subprocess.STDOUT, shell=True)
 
-    os.chdir(old_dir)
     if ret_code and no_errors:
         raise Exception(f"Execute shell command error: ({program}): {ret_code}")
     return ret_code
 
 
-def shell_command_ret(directory, program, args=None):
+def shell_command_ret(program, args=None):
     if args is None:
         args = []
     if isinstance(args, str):
@@ -35,17 +31,32 @@ def shell_command_ret(directory, program, args=None):
         args = [f'"{arg}"' for arg in args]
     args.insert(0, program)
 
-    old_dir = os.getcwd()
-    os.chdir(old_dir)
-
     result_data = {"stdout": "", "stderr": ""}
 
     popen = subprocess.Popen(" ".join(args), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    stdout, stderr = popen.communicate()
-    popen.wait()
-    result_data['stdout'] = stdout.strip().decode('utf-8', errors='ignore')
-    result_data['stderr'] = stderr.strip().decode('utf-8', errors='ignore')
-
-    os.chdir(old_dir)
+    try:
+        stdout, stderr = popen.communicate()
+        popen.wait()
+        result_data['stdout'] = stdout.strip().decode('utf-8', errors='ignore')
+        result_data['stderr'] = stderr.strip().decode('utf-8', errors='ignore')
+    finally:
+        popen.stdout.close()
+        popen.stderr.close()
 
     return result_data
+
+
+def shell_command_in_dir(directory, program, args, no_errors=False):
+    old_dir = os.getcwd()
+    os.chdir(directory)
+    ret = shell_command(program, args, no_errors)
+    os.chdir(old_dir)
+    return ret
+
+
+def shell_command_ret_in_dir(directory, program, args, no_errors=False):
+    old_dir = os.getcwd()
+    os.chdir(directory)
+    ret = shell_command_ret(program, args)
+    os.chdir(old_dir)
+    return ret
