cfa13800e87e892614a253371896ccbe6ffda199
Depish <depish.eskry@yandex.ru>
2023-01-29 01:00:01 +0300

    Add more git commands

--- scripts/simple_git.py
+++ scripts/simple_git.py
@@ -1,5 +1,5 @@
 import os
-import shell_command
+from scripts import shell_command
 
 
 def has_git(directory):
@@ -39,11 +39,62 @@ def git_create_branch(directory, branch):
     shell_command.shell_command_in_dir(directory, "git", ["checkout", "-b", branch])
 
 
+def git_clone(directory, url):
+    """Run command 'git clone <url>'"""
+    shell_command.shell_command_in_dir(directory, "git", ["clone", url])
+
+
+def git_commit_hash(directory):
+    """Run command 'git rev-parse HEAD'"""
+    res = shell_command.shell_command_ret_in_dir(directory, "git", ["rev-parse", "HEAD"])
+    if res['stdout']:
+        return res['stdout']
+    return None
+
+
+def git_tag(directory, tag_name, tag_message):
+    """Run command 'git tag -a <tag_name> -m <tag_message>'"""
+    shell_command.shell_command_in_dir(directory, "git", ["tag", "-a", tag_name, "-m", tag_message])
+
+
+def git_list_branch(directory):
+    """Run command 'git branch -a'"""
+    res = shell_command.shell_command_ret_in_dir(directory, "git", ["branch", "-a"])
+    ret_value = []
+
+    if res['stdout']:
+        split_list = res['stdout'].split('\n')
+        for branch in split_list:
+            ret_value.append(branch.replace('\r', '').replace('*', '').strip())
+
+    return ret_value
+
+
+def git_current_branch(directory):
+    """Run command 'git branch -a' and find branch with *"""
+    res = shell_command.shell_command_ret_in_dir(directory, "git", ["branch", "-a"])
+
+    if res['stdout']:
+        split_list = res['stdout'].split('\n')
+        for branch in split_list:
+            if branch.startswith('*'):
+                return branch.replace('*', '').replace('\r', '').strip()
+
+    return None
+
+
+def git_init(directory):
+    """Run command 'git init'"""
+    shell_command.shell_command_in_dir(directory, "git", ["init"])
+
+
 class SimpleGit:
-    def __int__(self, directory):
+    def __int__(self, directory, init_repo_if_not_exists=False):
         self._directory = directory
-        if not has_git(self._directory):
+        if not has_git(self._directory) and not init_repo_if_not_exists:
             raise Exception(f"Folder {directory} is not git repository!")
+        elif not has_git(self._directory):
+            git_init(self._directory)
 
     def pull(self):
         git_fetch_and_pull(self._directory)
@@ -59,3 +110,15 @@ class SimpleGit:
 
     def create_branch(self, branch):
         git_create_branch(self._directory, branch)
+
+    def hash(self):
+        return git_commit_hash(self._directory)
+
+    def tag(self, tag_name, tag_message):
+        git_tag(self._directory, tag_name, tag_message)
+
+    def branch_list(self):
+        return git_list_branch(self._directory)
+
+    def current_branch(self):
+        return git_current_branch(self._directory)
